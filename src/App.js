c3c0428a0ebcf1ab903ac814c06fd4171b8fd67b
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-01-28 13:48:20 -0500

    initial wireframe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./degods.png";
+import "./App.css";
+import Card from "./components/UI/Card";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="py-2 text-2xl font-bold">
+          The DeGods Experiment Timeline is here.<br></br>
+          Understand our history to see into our future.
         </p>
+      </header>
+      <body>
+          <Card>DeGods Founded: 9/27/21</Card>
+          <Card>Big Step 2</Card>
+          <Card>Big Step 3</Card>
+      </body>
+      <footer className="">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="text-sm underline"
+          href="https://www.degods.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit DeGods.com
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  content: [
+    "./public/*",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      colors: {
+        'degods-lite': '#ffece8'
+      }
+    },
+  },
+  plugins: [],
+}
