5c6e73153ad6a3cf22612141b409d5262bb29342
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-01-29 22:22:12 -0500

    remove test photo from 1st item

--- src/components/Events/eventData.js
+++ src/components/Events/eventData.js
@@ -8,12 +8,12 @@ const eventData = [
       "In the heat of NFT summer, the DeGods plotted their reveal at the first sign of summer equinox.",
     cardDetailedText:
       "Frank began typing on this fateful day, and the keystrokes echoed into the cosmos with each successive new line and its eventual backspace. A Discord was formed, a team gathered, and a vision for absolute debauchery was formed on September the 21st, 2021.",
-    media: {
-      type: "IMAGE",
-      source: {
-        url: "http://someurl/image.jpg",
-      },
-    },
+    // media: {
+    //   type: "IMAGE",
+    //   source: {
+    //     url: "#",
+    //   },
+    // },
   },
   {
     title: "09/21/2021",
