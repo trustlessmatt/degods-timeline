21efa24b2f3df7cba5e9d96676216f630954ac43
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-01-30 19:37:02 -0500

    updated real data

--- src/components/Events/eventData.js
+++ src/components/Events/eventData.js
@@ -1,13 +1,12 @@
+import frankIsTyping from '../Assets/frank_is_typing.png';
 
 const eventData = [
   {
-    title: "09/21/2021",
-    cardTitle: "DeGods Established",
-    url: "http://www.degods.com/",
-    cardSubtitle:
-      "In the heat of NFT summer, the DeGods plotted their reveal at the first sign of summer equinox.",
+    title: "March 2021",
+    cardTitle: "DEGODS IS BORN",
+    cardSubtitle: "The Degods Twitter account created.",
     cardDetailedText:
-      "Frank began typing on this fateful day, and the keystrokes echoed into the cosmos with each successive new line and its eventual backspace. A Discord was formed, a team gathered, and a vision for absolute debauchery was formed on September the 21st, 2021.",
+      "The creation of its official twitter account @DeGodsNFT marked the birth of an idea; an experiment in deflationary NFTs using game theory that would take the Solana NFT space by storm. The landscape truly was not prepared for what DeGods would bring. During this time, the team was created and the planning phase began.",
     // media: {
     //   type: "IMAGE",
     //   source: {
@@ -16,47 +15,44 @@ const eventData = [
     // },
   },
   {
-    title: "09/21/2021",
-    cardTitle: "DeGods Established",
-    url: "http://www.degods.com/",
+    title: "09/16/2021",
+    cardTitle:
+      "PAPERHAND BITCH TAX REVEALED IN FIRST TWEET & DISCORD ANNOUNCEMENT",
+    url: "https://twitter.com/DeGodsNFT/status/1438671756001099781?s=20&t=oz97jzW2FurwoVooRUd5Eg",
     cardSubtitle:
       "In the heat of NFT summer, the DeGods plotted their reveal at the first sign of summer equinox.",
     cardDetailedText:
-      "Frank began typing on this fateful day, and the keystrokes echoed into the cosmos with each successive new line and its eventual backspace. A Discord was formed, a team gathered, and a vision for absolute debauchery was formed on September the 21st, 2021.",
+      'With its first announcements released on Twitter and Discord, Frank and the Degods Core Team opened the gates of Solympus to the DeGods community anxious to enter. One announcement in particular, the Paper Hand Bitch Tax (PHBT) sparked a chain-wide discussion and inspired projects across Solana to react and respond to this contreversial idea - a 33.3% tax on paper hands that sold below a specified floor price. The team had established themselves and the community as "habitual line-steppers" (RIP Charlie Murphy) prepared to question the status quo at every turn.',
     media: {
       type: "IMAGE",
       source: {
-        url: "http://someurl/image.jpg",
+        url: "https://pbs.twimg.com/media/E_cVc65VUAA4JVU?format=jpg&name=small",
       },
     },
   },
   {
-    title: "09/21/2021",
-    cardTitle: "DeGods Established",
-    url: "http://www.degods.com/",
-    cardSubtitle:
-      "In the heat of NFT summer, the DeGods plotted their reveal at the first sign of summer equinox.",
+    title: "09/18/2021",
+    cardTitle: "FIRST BOUNTIES LAUNCHED",
+    url: "https://discord.com/channels/886405884060786689/886405884060786692/889027263868317696",
     cardDetailedText:
-      "Frank began typing on this fateful day, and the keystrokes echoed into the cosmos with each successive new line and its eventual backspace. A Discord was formed, a team gathered, and a vision for absolute debauchery was formed on September the 21st, 2021.",
+      "The team continued to defy norms by requiring members of the early community to complete \"bounties\" that would gain them presale access and, in rare cases, airdrops. This was in huge contrast to most projects that encouraged their members to spam invites all over the internet to gain early access to whitelists and presales. Brilliantly, this move worked in everyone's favor, as twitter became a hub for creative expression and memorable antics. And to top it all off, it was all really powerful marketing. At this time, @aussienftguy (at the time, @kykerkow) was recognized for the first of many times for his dedication to the bounties and won the bounty airdrop. If you don't know Aussie, where the hell have you been?",
     media: {
       type: "IMAGE",
       source: {
-        url: "http://someurl/image.jpg",
+        url: "https://pbs.twimg.com/media/E_oGD61UYAAKqb5?format=jpg&name=large",
       },
     },
   },
   {
-    title: "09/21/2021",
-    cardTitle: "DeGods Established",
-    url: "http://www.degods.com/",
-    cardSubtitle:
-      "In the heat of NFT summer, the DeGods plotted their reveal at the first sign of summer equinox.",
+    title: "09/22/2021",
+    cardTitle: "THE PRESALE GAUNTLET (PROMISE, HEARTBREAK, AND SLEEPLESSNESS)",
+    url: "https://discord.com/channels/886405884060786689/886405884060786692/890263510070730782",
     cardDetailedText:
-      "Frank began typing on this fateful day, and the keystrokes echoed into the cosmos with each successive new line and its eventual backspace. A Discord was formed, a team gathered, and a vision for absolute debauchery was formed on September the 21st, 2021.",
+      "On the day of the first presale, the team ran into multiple issues with their NFT generator (Candy Machine by Metaplex), which caused Frank to have to announce a 12-hour delay. This moved the mint to 9PM PST / 12AM EST, leaving some people in Europe to stay up well into the morning! Like any good Solana mint, there was FUD, some tears, and a lot of mean things said about Frank's mom. This was also the genesis of the now-infamous \"Frank is typing...\" meme that we often see in the DeGods circle, as Frank slowly typed out (with what I can only infer was a combination of dread and panic) the news of the moment. The mint was continuously pushed past 9PM PST until the doors opened suddenly with many (including the authors of this webpage) asleep at their computers. The dedication so many had to being a part of this experiment speaks volumes to the belief in what they were trying to achieve.",
     media: {
       type: "IMAGE",
       source: {
-        url: "http://someurl/image.jpg",
+        url: {frankIsTyping},
       },
     },
   },
