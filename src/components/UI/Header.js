3ba937ff174a3430c3d2573dc9a273747985adcf
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-01-30 19:36:42 -0500

    working on header + assets

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,8 +1,24 @@
+import logo from "../Assets/degods.png";
+import twitter from "../Assets/twitter.png";
+
 const Header = () => {
   return (
-    <div>
+    <div className="p-2">
       {/* header left */}
+      <div className="flex items-center px-2">
+        {/* twitter */}
+        <a href="http://www.twitter.com/degodsnft" className="color">
+          <img src={twitter} width={50}></img>
+        </a>
+        {/* discord */}
+        {/* magic eden */}
+      </div>
+
       {/* header center */}
+      <div className="flex justify-center">
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+
       {/* header right */}
     </div>
   );
