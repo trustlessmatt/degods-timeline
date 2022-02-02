3c7fcc5c4fd457c6764b6990c888c3206b29cd69
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-02-01 19:04:28 -0500

    header finished and more content added

--- src/components/Events/eventData.js
+++ src/components/Events/eventData.js
@@ -2,15 +2,14 @@ const eventData = [
   {
     title: "March 2021",
     cardTitle: "DEGODS IS BORN",
-    cardSubtitle: "The Degods Twitter account created.",
     cardDetailedText:
       "The creation of its official twitter account @DeGodsNFT marked the birth of an idea; an experiment in deflationary NFTs using game theory that would take the Solana NFT space by storm. The landscape truly was not prepared for what DeGods would bring. During this time, the team was created and the planning phase began.",
-    // media: {
-    //   type: "IMAGE",
-    //   source: {
-    //     url: "#",
-    //   },
-    // },
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "https://pbs.twimg.com/media/FKjDVw7WYAA8kBc?format=jpg&name=small",
+      },
+    },
   },
   {
     title: "09/16/2021",
@@ -44,7 +43,7 @@ const eventData = [
     cardTitle: "THE PRESALE GAUNTLET (PROMISE, HEARTBREAK, AND SLEEPLESSNESS)",
     url: "https://discord.com/channels/886405884060786689/886405884060786692/890263510070730782",
     cardDetailedText:
-      "On the day of the first presale, the team ran into multiple issues with their NFT generator (Candy Machine by Metaplex), which caused Frank to have to announce a 12-hour delay. This moved the mint to 9PM PST / 12AM EST, leaving some people in Europe to stay up well into the morning! Like any good Solana mint, there was FUD, some tears, and a lot of mean things said about Frank's mom. This was also the genesis of the now-infamous \"Frank is typing...\" meme that we often see in the DeGods circle, as Frank slowly typed out (with what I can only infer was a combination of dread and panic) the news of the moment. The mint was continuously pushed past 9PM PST until the doors opened suddenly with many (including the authors of this webpage) asleep at their computers. The dedication so many had to being a part of this experiment speaks volumes to the belief in what they were trying to achieve.",
+      'On the day of the first presale, the team ran into multiple issues with their NFT generator (Candy Machine by Metaplex), which caused Frank to have to announce a 12-hour delay. This moved the mint to 9PM PST / 12AM EST, leaving some people in Europe to stay up well into the morning! Like any good Solana mint, there was FUD, some tears, and a lot of mean things said about Frank\'s mom. This was also the genesis of the now-infamous "Frank is typing..." meme that we often see in the DeGods circle, as Frank slowly typed out (with what I can only infer was a combination of dread and panic) the news of the moment. The mint was continuously pushed past 9PM PST until the doors opened suddenly with many (including the authors of this webpage) asleep at their computers. The dedication so many had to being a part of this experiment speaks volumes to the belief in what they were trying to achieve.',
     media: {
       type: "IMAGE",
       source: {
@@ -53,19 +52,83 @@ const eventData = [
     },
   },
   {
+    title: "09/25/2021",
+    cardTitle: "FIRST PREVIEW OF THE DEGODS CUSTOM MARKETPLACE",
+    url: "https://discord.com/channels/886405884060786689/886405884060786692/891502131494936646",
+    cardDetailedText:
+      "The DeGods custom marketplace was one of the original project deliverables for the team that turned into a Hydra's head situation. The marketplace was to be the domain of the DeGods, where we could implement the 33.3% PHBT without having to have another secondary marketplace implement it within their own projects. These are Frank's first publicly shared screenshots of this project.",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "https://media.discordapp.net/attachments/886405884060786692/891502126872809492/NFT_Buy_Card.jpeg?width=833&height=506",
+      },
+    },
+  },
+  {
+    title: "09/25/2021",
+    cardTitle: "FIRST DEGOD BURNED",
+    url: "https://twitter.com/DeGodsNFT/status/1441871746664001542",
+    cardDetailedText:
+      "In order to generate some hype and show the crypto space what DeGods is all about, Frank revealed a video showing their burn process manually - something that would soon become automated (and a staple of the brand). The video shows a DeGod being sold under the floor price and being sent to the burn wallet (note the address: 1nc1nerator11111...). The real note here was that 33.3% of the sale price went straight to the DeGods wallet, funding the burn bot to remove more floor DeGods from the supply. This was the core tenet of their experiment with deflationary NFTs!",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "https://img.search.brave.com/7_BxLSKiO4kUg1IJYXmWKSbFIzDlnVM__67Eisvs84Q/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9wbmdp/bWcuY29tL3VwbG9h/ZHMvZmlyZS9maXJl/X1BORzYwNDIucG5n",
+      },
+    },
+  },
+  {
     title: "09/26/2021",
     cardTitle: "AUSSIE EATS 100 NUGS ON LIVESTREAM AND BECOMES LEGEND",
     url: "https://twitter.com/frankdegods/status/1442332223470850054?s=20&t=UVMnh-xgiD3MPUShBfKnCw",
     cardDetailedText:
-      "For some reasons unknown to man or science, @aussienftguy talked about DeGods for an hour on Twitch while eating 100 lukewarm chicken nuggets. He had already cemented himself in the DeGods community as the guy who would go to lengths far greater than most to prove himself to the Core Team, and this was just icing on the cake (or sauce on the nugs? sorry) for all of us. This MFer became an instant classic and we love him to death.",
-      media: {
-        type: "IMAGE",
-        source: {
-          url: "https://pbs.twimg.com/media/FKZIAQDXsAITpiB?format=png&name=small",
-        },
+      "For reasons unknown to man or science, @aussienftguy talked about DeGods for an hour on Twitch while eating 100 lukewarm chicken nuggets. He had already cemented himself in the DeGods community as the guy who would go to lengths far greater than most to prove himself to the Core Team, and this was just icing on the cake (or sauce on the nugs? sorry) for all of us. This MFer became an instant classic and we love him to death.",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "https://pbs.twimg.com/media/FKZIAQDXsAITpiB?format=png&name=small",
       },
+    },
   },
-
+  {
+    title: "10/02/2021",
+    cardTitle: "SOLYMPUS AND HOW THE CEO OF SOLANA LABS MADE DEGODS HIS PFP",
+    url: "https://twitter.com/aeyakovenko/status/1444342055967223813",
+    cardDetailedText:
+      "The DeGods know how to make noise. During the Solympus marketing effort, we were able to collectively get the attention of big names in the Solana ecosystem, including none other than the Cofounder of Solana Labs, Anatoly Yakovenko. He accepted his custom 1/1 DeGod and made it his Twitter PFP for (from my memory) well over a week. All this hype and positive press right ahead of public mint caused DeGods to land at #22 trending on Twitter (!!) and gave DeGods a huge name to live up to.",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "https://pbs.twimg.com/media/FAtrjIlUUAAFu9l?format=jpg&name=small",
+      },
+    },
+  },
+  {
+    title: "10/08/2021",
+    cardTitle: "A PERFECT MINT FOR A DEGOD",
+    url: "https://discord.com/channels/886405884060786689/886405884060786692/896161557271154718",
+    cardDetailedText:
+      "Public mint went smoothly. No hiccups. Okay fine, it was a shit show. With all the pieces put into place, the community anxiously awaited the 3PM PST go-live time. All the while, scammers, bots, and FUD plagued the internet in a cosmic battle of good and evil. The mint site was announced ahead of time, with the password being dropped right at 3PM PST. Unfortunately, the DeGods Discord was attacked with bots right before launch, so an executive decision was made to only release the password on Twitter. The problem was: this change was announced on Discord at a time where bots were causing chaos, and many people didn't see the message in time. With the public mint selling out in moments, this left many people full of FUD, calling the project a rug, thowing spirits to an all time low.",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "https://pbs.twimg.com/media/FAtrjIlUUAAFu9l?format=jpg&name=small",
+      },
+    },
+  },
+  {
+    title: "10/08/2021",
+    cardTitle: '"THE MARKETPLACE WILL BE LIVE BEFORE THE END OF THE DAY"',
+    url: "https://discord.com/channels/886405884060786689/886405884060786692/896161557271154718",
+    cardDetailedText:
+      "In the aftermath of the mint day chaos, those who did mint were now anxiously awaiting the custom DeGods marketplace to paperhand their blue chip NFT. Frank boldly stated that the \"marketplace is coming before the end of the day\". As things inevitably got delayed, the team decided that they would be sending SOL to random holders every hour until the site went live. All the while, fake DeGods NFTs were being sold to those of us with half a brain, and then those same people tried flipping them, getting hit with the PHBT and filling up the Incinerator wallet with fakes. Welp, they say the best projects had the shittiest launches, just look at Degenerate Ape Academy!",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "https://pbs.twimg.com/media/FAtrjIlUUAAFu9l?format=jpg&name=small",
+      },
+    },
+  },
 ];
 
 export default eventData;
--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -5,30 +5,33 @@ import magiceden from "../Assets/magiceden.png";
 const Header = () => {
   return (
     // outer header container
-    <div className="sticky top-0 z-50 shadow-lg bg-degods-purp items-center p-5">
-      {/* header left */}
+    <div className="sticky top-0 z-50 shadow-lg bg-degods-purp flex p-4">
+      {/* header left - social icons */}
       <div className="flex items-center p-1">
         {/* twitter */}
-        <div className="rounded-lg mx-2 hover:border-b-2 hover:border-white">
-          <a href="http://www.twitter.com/degodsnft" className="">
+        <div className="rounded-lg mx-2">
+          <a href="http://www.twitter.com/degodsnft">
             <img src={twitter} width={50}></img>
           </a>
         </div>
         {/* discord */}
-        <div className="rounded-lg mx-2 hover:border-b-2 hover:border-white">
-          <a href="http://www.discord.gg/degods" className="">
+        <div className="rounded-lg mx-2">
+          <a href="http://www.discord.gg/degods">
             <img src={discord} width={50}></img>
           </a>
         </div>
         {/* magic eden */}
-        <div className="rounded-lg mx-2 p-3 hover:border-b-2 hover:border-white">
-          <a href="http://www.magiceden.io/marketplace/degods" className="">
+        <div className="rounded-lg mx-2 p-3">
+          <a href="http://www.magiceden.io/marketplace/degods">
             <img src={magiceden} width={45}></img>
           </a>
         </div>
       </div>
 
-      {/* header right */}
+      {/* header right: will be a Connect to Wallet button
+      <div className="items-center p-1 justify-end hover:cursor-not-allowed">
+          <div className="rounded-lg bg-white p-3">Connect Wallet</div>
+      </div> */}
     </div>
   );
 };
