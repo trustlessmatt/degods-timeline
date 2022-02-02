ec57caf0608e19719a3ea489cc319d866917f0ea
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-02-01 22:16:44 -0500

    More event data

--- src/components/Events/eventData.js
+++ src/components/Events/eventData.js
@@ -1,7 +1,7 @@
 const eventData = [
   {
     title: "March 2021",
-    cardTitle: "DEGODS IS BORN",
+    cardTitle: "THE DEGODS ARE BORN",
     cardDetailedText:
       "The creation of its official twitter account @DeGodsNFT marked the birth of an idea; an experiment in deflationary NFTs using game theory that would take the Solana NFT space by storm. The landscape truly was not prepared for what DeGods would bring. During this time, the team was created and the planning phase began.",
     media: {
@@ -104,6 +104,19 @@ const eventData = [
     },
   },
   {
+    title: "10/07/2021",
+    cardTitle: "ROADMAP RELEASED",
+    url: "https://roadmap.degods.com",
+    cardDetailedText:
+      'Roadmap v1 was released on a spiffy interactive web page that promised the following: a DeGods marketplace, rarity tools, Twitter integration, "Clout" AKA de.xyz - a launchpad to basically make DeGods holders famous and eventually a DAO. While some of these items haven\'t yet been released (or have been discontinued like the Marketplace - more on that later), we do have rarity.degods.com and a really dope DAO.',
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "#",
+      },
+    },
+  },
+  {
     title: "10/08/2021",
     cardTitle: "A PERFECT MINT FOR A DEGOD",
     url: "https://discord.com/channels/886405884060786689/886405884060786692/896161557271154718",
@@ -112,7 +125,7 @@ const eventData = [
     media: {
       type: "IMAGE",
       source: {
-        url: "https://pbs.twimg.com/media/FAtrjIlUUAAFu9l?format=jpg&name=small",
+        url: "#",
       },
     },
   },
@@ -121,11 +134,114 @@ const eventData = [
     cardTitle: '"THE MARKETPLACE WILL BE LIVE BEFORE THE END OF THE DAY"',
     url: "https://discord.com/channels/886405884060786689/886405884060786692/896161557271154718",
     cardDetailedText:
-      "In the aftermath of the mint day chaos, those who did mint were now anxiously awaiting the custom DeGods marketplace to paperhand their blue chip NFT. Frank boldly stated that the \"marketplace is coming before the end of the day\". As things inevitably got delayed, the team decided that they would be sending SOL to random holders every hour until the site went live. All the while, fake DeGods NFTs were being sold to those of us with half a brain, and then those same people tried flipping them, getting hit with the PHBT and filling up the Incinerator wallet with fakes. Welp, they say the best projects had the shittiest launches, just look at Degenerate Ape Academy!",
+      'In the aftermath of the mint day chaos, those who did mint were now anxiously awaiting the custom DeGods marketplace to paperhand their blue chip NFT. Frank boldly stated that the "marketplace is coming before the end of the day". As things inevitably got delayed, the team decided that they would be sending SOL to random holders every hour until the site went live. All the while, fake DeGods NFTs were being sold to those of us with half a brain, and then those same people tried flipping them, getting hit with the PHBT and filling up the Incinerator wallet with fakes. Welp, they say the best projects had the shittiest launches, just look at Degenerate Ape Academy!',
     media: {
       type: "IMAGE",
       source: {
-        url: "https://pbs.twimg.com/media/FAtrjIlUUAAFu9l?format=jpg&name=small",
+        url: "#",
+      },
+    },
+  },
+  {
+    title: "10/09/2021",
+    cardTitle: "BURN WALLET REACHES 950 SOL, MOSTLY ON SALES OF FAKE DEGODS",
+    url: "https://discord.com/channels/886405884060786689/886405884060786692/896563833374650388",
+    cardDetailedText:
+      "You really cannot make this shit up. The marketplace was postponed, and there were no verified places to list DeGods on secondary, but that didn't stop hoards of degens looking to make a quick flip from unverified collections and P2P trades. The burn wallet reached 950 SOL, most of which were from sales of fake DeGods to those of us who lack patience, the most rare virtue of them all. This was approximately $400,000+ in trading volume, without a marketplace. WTF! Frank took this opportunity to remind people to be safe and that they should wait until a verified location to safely make sales was established. All the while, DeGods was dumping SOL on the community as their own PHBT while the Marketplace was being debugged.",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "#",
+      },
+    },
+  },
+  {
+    title: "10/10 - 10/14/2021",
+    cardTitle: "THE MARKETPLACE LAUNCHES AND THEN BECOMES A PRISON, #FREEMANOJ",
+    url: "https://discord.com/channels/886405884060786689/886405884060786692/896890425288302595",
+    cardDetailedText:
+      "The Marketplace launches and then immediately crashes. First, an issue with the RPC endpoints, whatever that means. Then, the sheer web traffic alone overloaded their servers. As people got in and began listing, others were constantly refreshing their pages and spamming the server with HTTP requests and... yeah you get the point. The team worked with GenesysGo to get their back end patched up so that volume could pick up, but during this time, people's listings got locked up on the site. This meant you put your DeGod into escrow with their Marketplace, but you couldn't get in again to delist if you wanted to. Eventually, there ended up being a few strong soldiers that were trapped in Marketplace Hell for a number of days, and the hashtag #FREEMANOJ started to pick up steam. They really do say that there is no such thing as bad publicity, and boy was this its own experiment in that. We laugh because we learn. By the 11th, DeGods had worked with Alpha Art to list the project with a functioning PHBT. Go AA! Trading began working as intended while the team worked to upgrade the Marketplace.",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "#",
+      },
+    },
+  },
+  {
+    title: "10/15/2021",
+    cardTitle: "PAPER HAND BITCH TAX FULLY DEPLOYED",
+    url: "https://discord.com/channels/886405884060786689/886405884060786692/898689805163069460",
+    cardDetailedText:
+      "The team spent hundreds of hours crafting the concept of the PHBT, and had numerous discussions with the community about how (and when) exactly to implement it. Do you only tax sales of DeGods that are lower than the current floor price? What about DeGods sold at a loss (list price > last purchase price)? Also, what to do about the Burn Bot? Does it snipe the floor? Or does it randomly buy DeGods all over the Marketplace at various price points? This was a discussion that was had with the community, and the DeGods were better for it. Finally, on this day, it was decided that they would do both: sales under the floor or lower than last purchase price would get hit with a 33.3% tax. Game on.",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "#",
+      },
+    },
+  },
+  {
+    title: "10/17/2021",
+    cardTitle: "1% OF ALL DEGODS BURNED",
+    url: "https://discord.com/channels/886405884060786689/886405884060786692/899469505569370113",
+    cardDetailedText: "'NUFF SAID.",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "#",
+      },
+    },
+  },
+  {
+    title: "10/20/2021",
+    cardTitle: "ANNOUNCEMENT OF DE.XYZ",
+    url: "https://discord.com/channels/886405884060786689/886405884060786692/900622484389384203",
+    cardDetailedText:
+      'The concept behind de.xyz was really clever. From Frank: "We are going to be the first NFT project to have our own community-owned content platform." Basically, DeGods owners get to publish their own content on this site, and own all their contributions. There were two ways to participate: creator or community member. Entrants would submit a proposal for valuable content to the DeGods Core Team via a Google doc and the community would support it by interacting with it, sharing it on social, and basically hyping it up the way DeGods do best.',
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "#",
+      },
+    },
+  },
+  {
+    title: "10/28/2021",
+    cardTitle: "ISLAND BOYS X DEGODS - AN ABSOLUTE BANGER",
+    url: "https://twitter.com/DeGodsNFT/status/1453843873399148549?s=20&t=n950oPIAaovsPhCHtIWbgA",
+    cardDetailedText:
+      "Just when everything was starting to calm down and look in control, DeGods dropped the #1 remix of 2021 featuring Island Boys. The DeGods x Island Boys collab almost crashed Twitter. You either love Island Boys in which case it was an incredible BANGER or you hated Island Boys and you retweeted it like a really good hate fuck. Things get really weird in crypto, eh?",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "#",
+      },
+    },
+  },
+  {
+    title: "11/03/2021",
+    cardTitle: "AUSSIE JUMPS OUT OF A FUCKING PLANE DRESSED AS A DEGOD",
+    url: "https://discord.com/channels/886405884060786689/886405884060786692/905577419715543101",
+    cardDetailedText:
+      "I MEAN... what do you even say about this? At this point, the team had hired @aussienftguy on full time to manage the community and build on it with his balls of absolute titanium. This wasn't for a bounty, this wasn't for a whitelist (he is known for getting tattooed for a good WL), this was just about being the best in the game. This stunt picked up some more traction and recognition in the community. \"Damn people really do be jumping out of planes for this NFT eh?\"",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "https://pbs.twimg.com/media/FDzdbqxUcAgx7Yb?format=jpg&name=large",
+      },
+    },
+  },
+  {
+    title: "01/31/2022",
+    cardTitle: "DEGODS STAKING GOES LIVE, LOCKING UP $5MM IN DEGENERATE JPEGS FOR SOME STARDUST",
+    url: "#",
+    cardDetailedText:
+      "$DUST staking goes live and within 24 hours, 50% of the total supply of DeGods were staked. This amounts to more than $5 million TVL in 24 hours, probably nothing. People are chomping at the bit to trade their $DUST for a chance to mutate their DeGod into a DeadGod, and staking is the only way to get there at this point. That is, until the burning mechanic goes live. The best part? Frank surprised the community with fucking PHBT2: unstaking your DeGod will cost you 0.0333 SOL. The PHBT lives on!",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "#",
       },
     },
   },
